b9340f8fefb0009612b6e533696d335b414fb8c2
Confucian <confucian.e@gmail.com>
2023-07-24 20:55:39 +0800

    feat: gas estimate

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,6 +1,12 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
 
+// gas fee estimate
+async function getFees() {
+  const response = await fetch('https://beaconcha.in/api/v1/execution/gasnow'); 
+  return response.text();
+}
+
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
@@ -14,18 +20,17 @@ import { panel, text } from '@metamask/snaps-ui';
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
+      return getFees().then(fees => {
+        return snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'alert',
+            content: panel([
+              text(`Hello, **${origin}**!`),
+              text(`Current gas fee estimates: ${fees}`),
+            ]),
+          }
+        });
       });
     default:
       throw new Error('Method not found.');
